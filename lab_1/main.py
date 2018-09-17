eecd15c20672c9473ccaac51220f632206d62d0d
mafatekhov <brutalduck@gmail.com>
2018-09-17 21:55:17 +0300

    Added return to calculate_frequencies function

--- lab_1/main.py
+++ lab_1/main.py
@@ -9,7 +9,7 @@ def calculate_frequences() -> dict:
     """
     Calculates number of times each word appears in the text
     """
-    pass
+    return {'word': 10}
 
 def filter_stop_words() -> dict:
     """
