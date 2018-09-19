6bc2c88d6f105781716a8226184ea8ee17d47afc
mfatekho <marat.fatekhov@intel.com>
2018-09-19 10:57:27 +0300

     Minor change

--- lab_1/main.py
+++ lab_1/main.py
@@ -9,7 +9,7 @@ def calculate_frequences() -> dict:
     """
     Calculates number of times each word appears in the text
     """
-    return {'word': 10, 'another_word': 30}
+    return {'word': 10}
 
 def filter_stop_words() -> dict:
     """
